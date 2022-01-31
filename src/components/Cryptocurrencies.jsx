b8396098aa057c76a9b185fc8d327e1b27cd0e8d
kiran kathar <kkathar4@gmail.com>
2022-02-01 00:17:43 +0530

    first commit

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -4,6 +4,8 @@ import { Link } from "react-router-dom";
 import { Card, Row, Col, Input } from "antd";
 import { useGetCryptosQuery } from "../services/cryptoApi";
 
+// comment to check github
+
 const Cryptocurrencies = ({ simplified }) => {
   const count = simplified ? 10 : 100;
   const { data: cryptosList, isFetching } = useGetCryptosQuery(count);
